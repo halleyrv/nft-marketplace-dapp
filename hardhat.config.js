dfb753f41305928f4bca2522891c3486eb24d3de
Halle <halley.rios@dowjones.com>
2024-06-05 15:43:01 -0500

    featured: deploy smart contract and import abi

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      }
+    }
+  },
+  networks:{
+    hardhat:{
+      chainId:31337,
+    }
+  }
+}
